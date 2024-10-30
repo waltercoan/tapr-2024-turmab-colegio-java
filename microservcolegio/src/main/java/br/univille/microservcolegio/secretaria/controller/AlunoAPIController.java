161c4fe60eec8edbafe831b9e42fdafc555c915c
Walter Silvestre Coan <walter.coan@gmail.com>
2024-10-30 01:16:15 +0000

    API POST

--- microservcolegio/src/main/java/br/univille/microservcolegio/secretaria/controller/AlunoAPIController.java
+++ microservcolegio/src/main/java/br/univille/microservcolegio/secretaria/controller/AlunoAPIController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,15 @@ public class AlunoAPIController {
         var listaAlunos = service.getAll();
 
         return new ResponseEntity<List<Aluno>>(listaAlunos,HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Aluno> post(@RequestBody Aluno aluno){
+        if(aluno == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        var alunoSalvo = service.save(aluno);
+
+        return new ResponseEntity<Aluno>(alunoSalvo, HttpStatus.OK);
     }
 
 }
